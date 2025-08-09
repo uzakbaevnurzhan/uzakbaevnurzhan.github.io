e2e9bf042fa7f7a586a2262cf46b09642bb9c63b
uzakbaevnurzhan <164526397+uzakbaevnurzhan@users.noreply.github.com>
2025-08-09 19:01:50 +0500

    Update index.jsx

--- OraculAi/pages/index.jsx
+++ OraculAi/pages/index.jsx
@@ -1,4 +1,5 @@
-import OraculAi from "../src/OraculAi";
+import dynamic from "next/dynamic";
+const OraculAi = dynamic(() => import("../src/OraculAi"), { ssr: false });
 
 export default function Home() {
   return <OraculAi />;
